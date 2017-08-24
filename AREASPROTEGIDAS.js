0458380ea6fea6a42263b23c36a2eba155383f2f
GisNorte <30350684+GisNorte@users.noreply.github.com>
2017-08-24 14:42:50 -0300

    Update AREASPROTEGIDAS.js

--- AREASPROTEGIDAS.js
+++ AREASPROTEGIDAS.js
@@ -1,4 +1,4 @@
-var areasprotegidas = {
+var AREASPROTEGIDAS = {
 "type": "FeatureCollection",
 "name": "AREAS PROTEGIDAS",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
