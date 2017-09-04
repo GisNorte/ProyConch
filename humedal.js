3754253bb505e425c7529eae19d5d39e31a011e9
GisNorte <30350684+GisNorte@users.noreply.github.com>
2017-09-04 13:30:28 -0300

    Add files via upload

---
+++ humedal.js
@@ -0,0 +1,8 @@
+var humedal = {
+"type": "FeatureCollection",
+"name": "humedal",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Santuario de la Naturaleza Humedal  Rio Lluta" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -70.32868092991724, -18.406385866200811 ], [ -70.327136632827958, -18.405059521218764 ], [ -70.317489872758131, -18.41709720950286 ], [ -70.318834592053648, -18.418488688485315 ], [ -70.323592396312037, -18.412597668196117 ], [ -70.32868092991724, -18.406385866200811 ] ] ] } }
+]
+}
